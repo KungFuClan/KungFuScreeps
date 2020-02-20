f3e141fe341966b9b5b0d16b9a7fa58ee33509b3
Jake <jcwaldrip@att.net>
2020-02-20 14:28:43 -0600

    added mem back

--- src/main.ts
+++ src/main.ts
@@ -9,9 +9,9 @@
 
 // Define prototypes
 import { ErrorMapper, ManagerManager } from "Utils/Imports/internals";
-import { MilitaryMovement_Helper } from "Military/Military.Movement.Helper";
-import { CostMatrixApi } from "Pathfinding/CostMatrix.Api";
+import { Mem } from "Utils/MemHack";
 export const loop = ErrorMapper.wrapLoop(() => {
-  ManagerManager.runManagerManager();
-  // CostMatrixApi.visualizeCostMatrix(new PathFinder.CostMatrix(), "W9N7");
+    Mem.loadCachedMemory();
+    ManagerManager.runManagerManager();
+    // CostMatrixApi.visualizeCostMatrix(new PathFinder.CostMatrix(), "W9N7");
 });
