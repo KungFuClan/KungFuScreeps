3b565987af8db18c81a078de02309f5939e8ba52
Jacob <jakeywaldrip@gmail.com>
2020-09-22 01:48:59 -0500

    wrote comments planning manager

--- src/AutoConstruction/AutoConstructionManager.ts
+++ src/AutoConstruction/AutoConstructionManager.ts
@@ -19,5 +19,14 @@ export class AutoConstructionManager {
      */
     private static runSingleAutoConstructionManager(room: Room): void {
 
+        // get the bunker center
+        // get the current rcl
+        // check if we're missing any buildings for the current rcl
+        // return these buildings as an array that we're missing
+        // object denoting it (ie source container, upgrader link, etc)
+        // opt param saying what object its near like for upgrader link and source containers
+        // loop over this array, attempting to place each building
+        // get a list of valid locations for the building
+        // place building on the first valid location
     }
 }
