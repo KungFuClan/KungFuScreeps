ce82b756c3ff92b720df901927e70f49f87c0b00
Jacob <jakeywaldrip@gmail.com>
2020-09-22 01:35:26 -0500

    wrote manager foundation

--- src/AutoConstruction/AutoConstructionManager.ts
+++ src/AutoConstruction/AutoConstructionManager.ts
@@ -1,3 +1,23 @@
+import { MemoryApi_Empire } from "Utils/Imports/internals";
+
 export class AutoConstructionManager {
 
+    /**
+     * Run the auto construction manager for all rooms
+     */
+    public static runAutoConstructionManager(): void {
+        const ownedRooms: Room[] = MemoryApi_Empire.getOwnedRooms();
+        for (const room of ownedRooms) {
+            if (!room) continue;
+            this.runSingleAutoConstructionManager(room);
+        }
+    }
+
+    /**
+     * Run auto construction manager for a single room
+     * @param room The room we are running it for
+     */
+    private static runSingleAutoConstructionManager(room: Room): void {
+
+    }
 }
