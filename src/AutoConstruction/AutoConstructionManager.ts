18e9f654034bd565c77d6c1fa3c01983eeb2ed4e
jwaldrip <jwaldrip@hlprd.com>
2020-09-28 15:57:00 -0500

    wrote out method calls for planning purposes

--- src/AutoConstruction/AutoConstructionManager.ts
+++ src/AutoConstruction/AutoConstructionManager.ts
@@ -1,4 +1,4 @@
-import { MemoryApi_Empire, MemoryApi_Room } from "Utils/Imports/internals";
+import { AutoConstruction_Api, MemoryApi_Empire, MemoryApi_Room } from "Utils/Imports/internals";
 
 export class AutoConstructionManager {
 
@@ -19,15 +19,21 @@ export class AutoConstructionManager {
      */
     private static runSingleAutoConstructionManager(room: Room): void {
         if (!room.controller) return;
+        const currentConstructionCount = Object.keys(Game.constructionSites).length;
+        if (currentConstructionCount >= MAX_CONSTRUCTION_SITES) return;
         const bunkerCenter: RoomPosition = MemoryApi_Room.getBunkerCenter(room);
+        const controller: StructureController = room.controller;
         const rcl: number = room.controller.level;
+        const sources: Source[] = MemoryApi_Room.getSources(room.name);
 
-        // check if we're missing any buildings for the current rcl
-        // return these buildings as an array that we're missing
-        // object denoting it (ie source container, upgrader link, etc)
-        // opt param saying what object its near like for upgrader link and source containers
-        // loop over this array, attempting to place each building
-        // get a list of valid locations for the building
-        // place building on the first valid location
+
+        // Run the checks for all buildings we will need to consider
+        AutoConstruction_Api.checkBunkerCenterBuildings(room, bunkerCenter, rcl, currentConstructionCount);
+        AutoConstruction_Api.checkControllerBuildings(room, controller, rcl, currentConstructionCount);
+        AutoConstruction_Api.checkExtensions(room, bunkerCenter, rcl, currentConstructionCount);
+        AutoConstruction_Api.checkRoads(room, bunkerCenter, rcl, currentConstructionCount);
+        AutoConstruction_Api.checkSourceBuildings(room, sources, rcl, currentConstructionCount);
+        AutoConstruction_Api.checkRemoteRoomSourceBuildings(room, currentConstructionCount);
+        AutoConstruction_Api.checkRemoteRoomRoads(room, currentConstructionCount);
     }
 }
