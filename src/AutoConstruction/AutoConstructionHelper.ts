cc6af14658531a1cd31e3c5b6598b644bfb3b1a2
jwaldrip <jwaldrip@hlprd.com>
2020-09-29 10:35:11 -0500

    added documentation to methods

--- src/AutoConstruction/AutoConstructionHelper.ts
+++ src/AutoConstruction/AutoConstructionHelper.ts
@@ -1,38 +1,79 @@
 // Helper for automatic construction for rooms
 export class AutoConstruction_Helper {
 
-    // AutoConstruction_Helper.checkTowers(room, bunkerCenter, rcl, currentConstructionCount);
-    // AutoConstruction_Helper.checkSpawns(room, bunkerCenter, rcl, currentConstructionCount);
-    // AutoConstruction_Helper.checkBunkerCenterLink(room, bunkerCenter, rcl, currentConstructionCount);
-    // AutoConstruction_Helper.checkStorage(room, bunkerCenter, rcl, currentConstructionCount);
-    // AutoConstruction_Helper.checkTerminal(room, bunkerCenter, rcl, currentConstructionCount);
-    // AutoConstruction_Helper.checkBunkcerCenterRamparts(room, bunkerCenter, rcl, currentConstructionCount);
-    // AutoConstruction_Helper.checkBunkerCenterRoads(room, bunkerCenter, rcl, currentConstructionCount);
-
+    /**
+     *
+     * @param room The room we are currently checking on
+     * @param bunkerCenter The bunker center of the room
+     * @param rcl the current rcl the room is on
+     * @param currentConstructionCount the current number of active construction sites empire wide
+     */
     public static checkTowers(room: Room, bunkerCenter: RoomPosition, rcl: number, currentConstructionCount: number): void {
 
     }
 
+    /**
+     *
+     * @param room The room we are currently checking on
+     * @param bunkerCenter The bunker center of the room
+     * @param rcl the current rcl the room is on
+     * @param currentConstructionCount the current number of active construction sites empire wide
+     */
     public static checkSpawns(room: Room, bunkerCenter: RoomPosition, rcl: number, currentConstructionCount: number): void {
 
     }
 
+    /**
+     *
+     * @param room The room we are currently checking on
+     * @param bunkerCenter The bunker center of the room
+     * @param rcl the current rcl the room is on
+     * @param currentConstructionCount the current number of active construction sites empire wide
+     */
     public static checkBunkerCenterLink(room: Room, bunkerCenter: RoomPosition, rcl: number, currentConstructionCount: number): void {
 
     }
 
+    /**
+     *
+     * @param room The room we are currently checking on
+     * @param bunkerCenter The bunker center of the room
+     * @param rcl the current rcl the room is on
+     * @param currentConstructionCount the current number of active construction sites empire wide
+     */
     public static checkStorage(room: Room, bunkerCenter: RoomPosition, rcl: number, currentConstructionCount: number): void {
 
     }
 
+    /**
+     *
+     * @param room The room we are currently checking on
+     * @param bunkerCenter The bunker center of the room
+     * @param rcl the current rcl the room is on
+     * @param currentConstructionCount the current number of active construction sites empire wide
+     */
     public static checkTerminal(room: Room, bunkerCenter: RoomPosition, rcl: number, currentConstructionCount: number): void {
 
     }
 
+    /**
+     *
+     * @param room The room we are currently checking on
+     * @param bunkerCenter The bunker center of the room
+     * @param rcl the current rcl the room is on
+     * @param currentConstructionCount the current number of active construction sites empire wide
+     */
     public static checkBunkerCenterRamparts(room: Room, bunkerCenter: RoomPosition, rcl: number, currentConstructionCount: number): void {
 
     }
 
+    /**
+     *
+     * @param room The room we are currently checking on
+     * @param bunkerCenter The bunker center of the room
+     * @param rcl the current rcl the room is on
+     * @param currentConstructionCount the current number of active construction sites empire wide
+     */
     public static checkBunkerCenterRoads(room: Room, bunkerCenter: RoomPosition, rcl: number, currentConstructionCount: number): void {
 
     }
