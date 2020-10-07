36b5a2c84c1fa683cf93ffa03bc766b699b957cc
Brock Sharp <Uhmbrock@hotmail.com>
2020-10-06 21:53:35 -0500

    Fixed double export in internals.ts of autoconstruct files.

--- src/Utils/Imports/internals.ts
+++ src/Utils/Imports/internals.ts
@@ -24,7 +24,6 @@ export * from "Room/Room.State.Api";
 export * from "Room/Room.Structure.Api";
 export * from "Spawn/Spawn.Api";
 export * from "Memory/Memory.Military.Api";
-export * from "AutoConstruction/AutoConstruction.Api";
 // ----------------------------
 // Export All Helpers
 export * from "AutoConstruction/AutoConstructionHelper";
@@ -38,7 +37,6 @@ export * from "Memory/MemoryHelper";
 export * from "Memory/Memory.Room.Helper";
 export * from "Military/militaryData.Helper";
 export * from "Military/Military.Intents.Helper";
-export * from "AutoConstruction/AutoConstructionHelper";
 // -----------------------------
 // Body/Option Helper Implementations
 export * from "Creeps/RoleHelpers/MinerBodyOptsHelper";
@@ -90,7 +88,7 @@ export * from "Memory/MemoryManagement";
 export * from "Room/RoomManager";
 export * from "Spawn/SpawnManager";
 export * from "Military/MilitaryManager";
-export * from 'AutoConstruction/AutoConstructionManager';
+export * from "AutoConstruction/AutoConstructionManager";
 // ---------------------------------
 // Import Room Visuals
 export * from "RoomVisuals/RoomVisual.Api";
